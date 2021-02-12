1a8d932626c2c4183a41eda1a17c2b1775a74d87
wyru <wsaymonoficial@gmail.com>
2021-02-12 14:22:16 -0300

    fix(login): upsert data on mongo db

--- pages/api/oauth/index.ts
+++ pages/api/oauth/index.ts
@@ -1,5 +1,28 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {MongoClient, Db} from 'mongodb';
+import  {URL}  from 'url';
+
+let cachedDB:Db = null;
+
+const getDBConnection  = async (uri:string) => {
+
+  if(cachedDB){
+    return cachedDB;
+  }
+  const client  = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+  });
+
+  const dbName = (new URL(uri)).pathname.substr(1);
+
+  const db =  client.db(dbName);
+
+  cachedDB = db;
+
+  return db;
+}
 
 const getToken = async (code:string|undefined) => {
   const {data} = await axios.post('https://github.com/login/oauth/access_token',
@@ -44,10 +67,30 @@ const handle = async (request:NextApiRequest, response:NextApiResponse) => {
     const code =  request.query.code as string; 
     const token  = await getToken(code);
     const userData = await getUserData(token);
+
+    const db =  await getDBConnection(process.env.MONGODB_URI);
+
+    const collection = db.collection('users');
+
+    const query = {
+      gitHubId: userData.gitHubId
+    }
+
+    const update = {
+      $set: userData
+    }
+
+    const options = {
+      upsert: true 
+    };
+
+    const result = await collection.updateOne(query, update, options);
+
     response.status(200).json({
-      userData
+      result: result.result
     });
   } catch (error) {
+    console.log("🐛 ~ file: index.ts ~ line 96 ~ handle ~ error", error)
     response.status(500).json({
       message: error.toString()
     });
